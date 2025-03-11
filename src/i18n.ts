c1f9d8f4c9ec100982799c1df8dcc17944133c4e
imranmehdi <imran.mehdi1992@gmail.com>
2025-03-11 09:37:04 +0100

    translations setup

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,26 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from './locales/en/common.json';
+import commonDe from './locales/de/common.json';
+import festivalsDe from './locales/de/festivals.json';
+import commonEn from './locales/en/common.json';
+import festivalsEn from './locales/en/festivals.json';
 
 const resources = {
+  de: {
+    common: commonDe,
+    festivals: festivalsDe,
+  },
   en: {
-    translation: {
-      ...en,
-    },
+    common: commonEn,
+    festivals: festivalsEn,
   },
 };
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: 'en',
-
+  lng: localStorage.getItem('language') || 'en',
+  ns: ['common', 'festivals'],
+  defaultNS: 'common',
   interpolation: {
     escapeValue: false,
   },
